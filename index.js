27602d0d3dddd6ceb3b224776501309ef64db9fb
Siendo Miguel <siendomiguel@gmail.com>
2020-11-06 18:01:24 -0500

    validacion del ready

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Discord = require("discord.js")
 const config = require("./config.json")
 
+client.on('ready', () => {
+    console.log('El Bot esta listo para usarse!');
+});
+
 const client = new Discord.Client();
 client.login(config.BOT_TOKEN)
