fd57c4e04a7c836f5f824428b2bbf0e82191c8cb
Siendo Miguel <siendomiguel@gmail.com>
2020-11-06 12:03:18 -0500

    Bot Creado

--- index.js
+++ index.js
@@ -4,17 +4,3 @@ const config = require("./config.json")
 const client = new Discord.Client();
 client.login(config.BOT_TOKEN)
 
-//Agregando Roles
-let miembro = message.mentions.members.first();
-let nombrerol = args.slice(1).join(' ');
-
-let role = message.guild.roles.cache.find(r => r.name === nombrerol);
-let perms = message.member.hasPermission("MANAGE_ROLES");
-
-if(!perms) return message.channel.send("No tienes permisos suficientes, para agregar roles.");
-if(!miembro) return message.reply('Debe mencionar a un miembro.');
-if(!nombrerol) return message.channel.send('Escriba el nombre del rol a agregar.');
-if(!role) return message.channel.send('Rol no encontrado en el servidor.');
-
-miembro.roles.add(role).catch(console.error);
-message.channel.send(`El rol **${role.name}** fue agregado a **${miembro.user.username}**.`);
