5159352f996913a85527eead439fb05af54ca3c0
Siendo Miguel <siendomiguel@gmail.com>
2020-11-06 21:30:41 -0500

    bot terminado

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const Discord = require("discord.js")
 const config = require("./config.json")
 
-client.on('ready', () => {
-    console.log('El Bot esta listo para usarse!');
-});
-
 const client = new Discord.Client();
 client.login(config.BOT_TOKEN)
+
+client.on('ready', () => {
+    console.log(`Ya esta encendido el Bot ${client.user.tag}!`);
+    client.user.setStatus('dnd');
+  });
+
+//Se debe de indicar que el Bot este prendido para comenzar los comandos
+client.on('message', message => {
+    //Comandos y mensajes
+    if (message.content === '!ping') {
+        //Esta es la respuesta del mensaje
+        message.channel.send('pong');
+    }
+
+    if (message.content === '!ayudame') {
+        message.channel.send('Este bot aun esta en fase de pruebas');
+        //Esto elimina el mensaje que se envia
+        message.delete();
+    }
+
+    if(message.content === '!nuevosingresos') {
+        message.channel.send('');
+        message.delete();
+
+    }
+
+});
